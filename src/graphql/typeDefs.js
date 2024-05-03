3ac43333065712242c522dfb1de1f0ad1c065544
Rody Huancas <rodyhuancas.04@gmail.com>
2024-05-02 20:19:34 -0500

    Add: muttations with graphql

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -4,4 +4,16 @@ export const typeDefs = gql`
   type Query {
     hello: String
   }
+
+  type Mutation {
+    createProject(name: String, description: String): Project
+  }
+
+  type Project {
+    _id: ID
+    name: String
+    description: String
+    createdAt: String
+    updatedAt: String
+  }
 `;
