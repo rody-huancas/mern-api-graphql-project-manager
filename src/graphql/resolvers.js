2344703b7b329a023aefb6eae36aa8296c292c89
Rody Huancas <rodyhuancas.04@gmail.com>
2024-05-07 19:13:00 -0500

    Add: validate task not found

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -40,7 +40,9 @@ export const resolvers = {
       return deletedProject;
     },
     deleteTask: async(_, {_id}) => {
-
+      const deletedTask = Task.findByIdAndDelete(_id);
+      if(!deletedTask) throw new Error("Task not found");
+      return deletedTask;
     },
   },
 };
