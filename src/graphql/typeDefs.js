89e6b9ebee22406d9e69780b5f52f8e6b676c413
Rody Huancas <rodyhuancas.04@gmail.com>
2024-05-07 19:19:16 -0500

    Add: project and task updated

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -12,8 +12,10 @@ export const typeDefs = gql`
   type Mutation {
     createProject(name: String, description: String): Project
     deleteProject(_id: ID): Project
+    updateProject(_id: ID, name: String!, description: String): Project
     createTask(title: String, projectId: ID): Task
     deleteTask(_id: ID): Task
+    updateTask(_id: ID, title: String!, projectId: ID!): Task
   }
 
   type Project {
