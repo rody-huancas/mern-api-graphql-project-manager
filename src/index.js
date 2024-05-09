2bb80a32a3b7224dfca4f28c1d773bb9b0abacae
Rody Huancas <rodyhuancas.04@gmail.com>
2024-05-08 20:32:55 -0500

    Add: barrel file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import { typeDefs } from "./graphql/typeDefs.js";
 import { connectDB } from "./db/db.js";
-import { resolvers } from "./graphql/resolvers.js";
 import { startApolloServer } from "./app.js";
+import { typeDefs, resolvers } from "./graphql";
 
 connectDB();
 
